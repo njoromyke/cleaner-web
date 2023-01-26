799f44a99b407df06114d034d34411ba316c780e
njoromyke <mykenjoroge01@gmail.com>
2023-01-26 20:10:25 +0300

    added login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { showNotification } from "../../helpers/utils/notification";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import Loader from "../../components/loader/Loader";
+import { auth } from "../../services/firebase";
 
 const Login = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    signInWithEmailAndPassword(auth, formData.email, formData.password)
+      .then((userCredential) => {
+        // Signed in
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        showNotification(errorMessage);
+      })
+      .finally(() => setLoading(false));
+  };
+
   return (
     <>
       <section className="">
+        {loading && <Loader />}
         <div className="container">
           <div className="row align-items-center justify-content-center">
             <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
               <h2 className="text-center">
                 <span className="theme-cl">Login</span> Now
               </h2>
-              <form className="border p-3 rounded">
+              <form className="border p-3 rounded" onSubmit={handleSubmit}>
                 <div className="form-group">
-                  <label>User Name *</label>
+                  <label>User Email *</label>
                   <input
-                    type="text"
+                    type="email"
                     className="form-control"
-                    placeholder="Username*"
+                    placeholder="Email*"
+                    onChange={handleChange}
+                    name="email"
                   />
                 </div>
 
@@ -27,22 +57,12 @@ const Login = () => {
                     type="password"
                     className="form-control"
                     placeholder="Password*"
+                    onChange={handleChange}
                   />
                 </div>
 
                 <div className="form-group">
                   <div className="d-flex align-items-center justify-content-between">
-                    <div className="flex-1">
-                      <input
-                        id="dd"
-                        className="checkbox-custom"
-                        name="dd"
-                        type="checkbox"
-                      />
-                      <label for="dd" className="checkbox-custom-label">
-                        Remember Me
-                      </label>
-                    </div>
                     <div className="eltio_k2">
                       <Link to="/sign-up">
                         Don't have an account?
